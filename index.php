8b8fc3d4e57304d6a630c436be545fea0c8f4ad8
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-04 14:38:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
   $conn = pg_connect(getenv("DATABASE_URL"));
-  $sql = "CREATE TABLE IF NOT EXISTS Users (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    isGuest BOOLEAN,
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )";
+  //$sql = "CREATE TABLE IF NOT EXISTS Users (
+  //  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  //  firstname VARCHAR(30) NOT NULL,
+  //  lastname VARCHAR(30) NOT NULL,
+  //  email VARCHAR(50),
+  //  isGuest BOOLEAN,
+  //  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  //  )";
+  if (!$conn) {
+    echo "Not connected";
+  }else{
+    echo "Connected";
+  }
   pg_query($conn, $sql);
 ?>
