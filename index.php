f52fd3a31c83e62db476fcd141c18997df88b73a
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-04 14:48:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
+<!DOCTYPE html>
+  <head>
+    <title>Insert data to PostgreSQL with php - creating a simple web application</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style>
+      li {
+        list-style: none;
+      }
+    </style>
+  </head>
+  <body>
+    <h2>Enter data into book table</h2>
+      <ul>
+        <form name="insert" action="insert.php" method="POST" >
+          <li>Book ID:</li><li><input type="text" name="bookid" /></li>
+          <li>Book Name:</li><li><input type="text" name="book_name" /></li>
+          <li>Price (USD):</li><li><input type="text" name="price" /></li>
+          <li>Date of publication:</li><li><input type="text" name="dop" /></li>
+          <li><input type="submit" /></li>
+        </form>
+      </ul>
+  </body>
+</html>
+
 <?php
   $conn = pg_connect(getenv("DATABASE_URL"));
-  //$sql = "CREATE TABLE IF NOT EXISTS Users (
-  //  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  //  firstname VARCHAR(30) NOT NULL,
-  //  lastname VARCHAR(30) NOT NULL,
-  //  email VARCHAR(50),
-  //  isGuest BOOLEAN,
-  //  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-  //  )";
-  if (!$conn) {
-    echo "Not connected";
-  }else{
-    echo "Connected";
-  }
-  pg_query($conn, $sql);
+  $query = "INSERT INTO book VALUES ('$_POST[bookid]','$_POST[book_name]',
+'$_POST[price]','$_POST[dop]')";
+  pg_query($conn, $query); 
 ?>
