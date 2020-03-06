89fd75f90fffef85b19cf488cedcd0143225fac7
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-06 22:48:42 +0800

    Fixed the connection

--- fetch.php
+++ fetch.php
@@ -2,7 +2,7 @@
 	$conn = pg_connect(getenv("DATABASE_URL"));
 	$query = "SELECT * FROM items"; 
 
-	$rs = pg_query($con, $query) or die("Cannot execute query: $query\n");
+	$rs = pg_query($conn, $query) or die("Cannot execute query: $query\n");
 
 	while ($row = pg_fetch_row($rs)) {
 		echo "$row[0]\n";
