6e5ee6b3c0924e4e1ef1d83a37c42b6df477c74e
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-06 22:42:18 +0800

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -2,10 +2,9 @@
 	$conn = pg_connect(getenv("DATABASE_URL"));
 	$query = "SELECT item FROM items";
 	pg_query($conn, $query); 
-	$result = "INSERT INTO items VALUES ('$_POST[add_item]')";
-	while ($row = pg_fetch_row($result)) {
-	echo "Item: $row[0]";
-	echo "<br />\n";
-}
+	while ($row = pg_fetch_row($query)) {
+		echo "Item: $row[0]";
+		echo "<br />\n";
+	}
 
 ?>
