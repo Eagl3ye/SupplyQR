410ac4e7f4a4580e3118233ff691cabeebb069b6
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-06 22:46:01 +0800

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,10 +1,11 @@
 <?php
 	$conn = pg_connect(getenv("DATABASE_URL"));
-	$query = "SELECT item FROM items";
-	pg_query($conn, $query); 
-	while ($row = pg_fetch_row($query)) {
-		echo "Item: $row[0]";
-		echo "<br />\n";
+	$query = "SELECT * FROM cars LIMIT 5"; 
+
+	$rs = pg_query($con, $query) or die("Cannot execute query: $query\n");
+
+	while ($row = pg_fetch_row($rs)) {
+		echo "$row[0]\n";
 	}
 
 ?>
