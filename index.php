a4d59d151bebf6312c85cdaa77ea9d00ea4516e5
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-06 21:56:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,4 @@
 	include("templates/header.html");
 	include("templates/body.html");
 	include("templates/footer.html");
-	$conn = pg_connect(getenv("DATABASE_URL"));
-	$query = "INSERT INTO book VALUES ('$_POST[bookid]','$_POST[book_name]', '$_POST[price]', '$_POST[dop]')";
-	pg_query($conn, $query); 
 ?>
