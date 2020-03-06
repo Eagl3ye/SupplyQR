cd7e8a4c57a966423fa2cd24bbf5c7900454fddb
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-06 22:56:44 +0800

    Reverrt

--- fetch.php
+++ fetch.php
@@ -2,8 +2,7 @@
 	$conn = pg_connect(getenv("DATABASE_URL"));
 	$query = "SELECT * FROM items"; 
 
-	$msg = "Database is empty";
-	$rs = pg_query($conn, $query) or echo "<script type='text/javascript'>alert('$msg');</script>";
+	$rs = pg_query($conn, $query) or die("Cannot execute query: $query\n");
 
 	while ($row = pg_fetch_row($rs)) {
 		echo "$row[0]\n";
