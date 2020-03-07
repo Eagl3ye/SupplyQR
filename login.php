3ea7f482a853ead0ab1d2bbd0267be1c1d9a62cb
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-08 02:42:38 +0800

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 		$email = $_POST['login_email'];
 		$pass = $_POST['login_pass'];
 		$conn = pg_connect(getenv("DATABASE_URL"));
-		$query = "SELECT pass FROM userdata WHERE email = $email";
+		$query = "SELECT pass FROM userdata WHERE email='$email'";
 
 		$rs = pg_query($conn, $query) or die("Cannot execute query: $query\n");
 		$valpass = pg_fetch_all($rs);
