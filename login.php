d5c1d5336722227e6dd590c4b734ad3e42756898
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-08 02:21:57 +0800

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 		$email = $_POST['login_email'];
 		$pass = $_POST['login_pass'];
 		$conn = pg_connect(getenv("DATABASE_URL"));
-		$query = "SELECT * FROM userdata WHERE email = $email";
+		$query = "SELECT * FROM userdata";
 
 		$rs = pg_query($conn, $query) or die("Cannot execute query: $query\n");
 
