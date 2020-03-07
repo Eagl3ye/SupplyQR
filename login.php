06d36f797f67c6c3b0f49537d9782efbb7a1387f
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-08 02:03:56 +0800

    Fixing login.php

--- login.php
+++ login.php
@@ -5,6 +5,8 @@
 		$query = "SELECT email FROM userdata WHERE email = $_POST['login_email'] AND password = $_POST['login_pass']"; 
 
 		$rs = pg_query($conn, $query) or die("Cannot execute query: $query\n");
-		echo "pg_fetch_result($rs)";
+
+		$row = pg_fetch_result($rs);
+		echo "$row\n";
 	}
 ?>
