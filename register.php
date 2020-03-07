68233645da4527eb144e30a50324e0ccefbdd956
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-08 01:31:32 +0800

    Update register.php

--- register.php
+++ register.php
@@ -11,7 +11,7 @@
 			$conn = pg_connect(getenv("DATABASE_URL"));
 			$query = "CREATE TABLE IF NOT EXISTS userdata (firstname varchar(255), lastname varchar(255), email varchar(255), pass varchar(255), rank int(4))";
 			pg_query($conn, $query); 
-			$query = "INSERT INTO userdata VALUES ('$_POST[fname]', '$_POST[lname]', '$_POST[email]', '$_POST[pass]', $rank)";
+			$query = "INSERT INTO userdata VALUES ('$_POST[fname]', '$_POST[lname]', '$_POST[email]', '$_POST[pass]', '$rank')";
 			pg_query($conn, $query); 
 			//$query = "CREATE TABLE IF NOT EXISTS inventory_users (id int NOT NULL AUTO_INCREMENT, firstname varchar(255) NOT NULL, lastname varchar(255) NOT NULL, email varchar(255) NOT NULL UNIQUE, password varchar(255) NOT NULL, rank int(4))";
 			//pg_query($conn, $query); 
