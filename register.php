3a9653933f2303a915c32f6441fde7299227ce80
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-07 22:23:03 +0800

    Added password validation in register form

--- register.php
+++ register.php
@@ -8,14 +8,16 @@
 		$confirmpass = $_POST['register_passconfirm'];
 		if($pass != $confirmpass)
 		{	
-			echo "<script>alert('Invalid Password')</script>";
+			echo "<script>alert('Invalid Password'); document.getElementById('reg_fname').value = '<?php $fname?>'; document.getElementById('reg_lname').value = '<?php $lname?>'; document.getElementById('reg_email').value = '<?php $email?>'; document.getElementById('reg_pass').value = ''; document.getElementById('reg_cpass').value = '';</script>";
+			
 			//$conn = pg_connect(getenv("DATABASE_URL"));
-			//$query = "CREATE TABLE IF NOT EXISTS users (firstname varchar(255)) NOT NULL, lastname varchar(255) NOT NULL, email varchar(255) NOT NULL UNIQUE, rank int(6)";
+			//$query = "CREATE TABLE IF NOT EXISTS users (id, firstname varchar(255)) NOT NULL, lastname varchar(255) NOT NULL, email varchar(255) NOT NULL UNIQUE, rank int(6)";
 			//pg_query($conn, $query); 
 			//$query = "INSERT INTO items VALUES ('$_POST[add_item]')";
 			//pg_query($conn, $query); 
 		}else{
-			header("Location: signup.php");
+			echo "<script>alert('Invalid Password')";
+			header("Location: index.php");
 			exit;
 		}
 	}
