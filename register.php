fe74c4f971c3f970661c2787e40c484afcb69e50
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-08 00:16:35 +0800

    Database fixing

--- register.php
+++ register.php
@@ -13,18 +13,18 @@
 			exit;
 		}else{
 			$conn = pg_connect(getenv("DATABASE_URL"));
-			$query = "CREATE TABLE IF NOT EXISTS users (id int NOT NULL AUTO_INCREMENT, firstname varchar(255)) NOT NULL, lastname varchar(255) NOT NULL, email varchar(255) NOT NULL UNIQUE, rank int(6)";
+			$query = "CREATE TABLE IF NOT EXISTS inventory_users (id int NOT NULL AUTO_INCREMENT, firstname varchar(255)) NOT NULL, lastname varchar(255) NOT NULL, email varchar(255) NOT NULL UNIQUE, rank int(6)";
 			pg_query($conn, $query); 
 
-			$query = "SELECT COUNT(email) FROM users WHERE email = '$email'";
+			$query = "SELECT COUNT(email) FROM inventory_users WHERE email = '$email'";
 			$rs = pg_fetch_all(pg_query($conn, $query)); 
 			echo '$rs';
 			//if($query)
 			//$query = "INSERT INTO items VALUES ('$_POST[add_item]')";
 			//pg_query($conn, $query);
 
-			header("Location: templates/registration_success.html");
-			exit;
+			//header("Location: templates/registration_success.html");
+			//exit;
 		}
 	}
 ?>
