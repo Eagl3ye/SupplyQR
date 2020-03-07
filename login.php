b0ec1a897ed8980c01dfd96dbaba7cd807ac3cfa
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-08 02:14:57 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,10 @@
 <?php
 	if(isset($_POST['gateway_login'])) 
 	{
+		$email = $_POST['login_email'];
+		$pass = $_POST['login_pass'];
 		$conn = pg_connect(getenv("DATABASE_URL"));
-		$query = "SELECT * FROM userdata WHERE email = $_POST['login_email'] AND password = $_POST['login_pass']"; 
+		$query = "SELECT * FROM userdata WHERE email = $email AND password = $pass"; 
 
 		$rs = pg_query($conn, $query) or die("Cannot execute query: $query\n");
 
