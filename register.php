7d28c3014afbc25e41949bd7560a8ace80ddf0ef
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-08 00:32:41 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,24 +1,23 @@
 <?php
 	if(isset($_POST['gateway_register'])) 
 	{
-		$fname = $_POST['register_fname'];
-		$lname = $_POST['register_lname'];
-		$email = $_POST['register_email'];
 		$pass = $_POST['register_pass'];
 		$confirmpass = $_POST['register_passconfirm'];
 		if($pass != $confirmpass)
 		{
-			echo "<script>alert('Invalid Password');</script>";
+			echo "<script>alert('Invalid Password')</script>";
 			header("Location: signup.php");
 			exit;
 		}else{
 			$conn = pg_connect(getenv("DATABASE_URL"));
-			$query = "CREATE TABLE IF NOT EXISTS inventory_users (id int NOT NULL AUTO_INCREMENT, firstname varchar(255)) NOT NULL, lastname varchar(255) NOT NULL, email varchar(255) NOT NULL UNIQUE, rank int(6)";
+			$query = "CREATE TABLE IF NOT EXISTS inventory_users (id int NOT NULL AUTO_INCREMENT, firstname varchar(255)) NOT NULL, lastname varchar(255) NOT NULL, email varchar(255) NOT NULL UNIQUE, password varchar(255) NOT NULL, rank int(4)";
 			pg_query($conn, $query); 
 
-			$query = "SELECT COUNT(email) FROM inventory_users WHERE email = '$email'";
-			$rs = pg_query($conn, $query); 
-			echo $rs;
+			$query = "INSERT INTO inventory_users VALUES ('$_POST[register_fname]', '$_POST[register_lname]', '$_POST[register_email]', '$_POST[register_pass]'), 3)";
+			pg_query($conn, $query); 
+			//$query = "SELECT COUNT(email) FROM inventory_users WHERE email = '$email'";
+			//$rs = pg_query($conn, $query); 
+			//echo $rs;
 			//if($query)
 			//$query = "INSERT INTO items VALUES ('$_POST[add_item]')";
 			//pg_query($conn, $query);
