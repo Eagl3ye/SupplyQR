7427d98570ed6df90c7e426e8f2768d1274d18ff
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-08 08:04:49 +0800

    Update login.php

--- login.php
+++ login.php
@@ -4,14 +4,14 @@
 		$email = $_POST['login_email'];
 		$pass = $_POST['login_pass'];
 		$conn = pg_connect(getenv("DATABASE_URL"));
-		$query = "SELECT pass FROM userdata WHERE email='$email'";
+		$query = "SELECT * FROM userdata";
 
 		$rs = pg_query($conn, $query) or die("Cannot execute query: $query\n");
 		$valpass = pg_fetch_row($rs);
-		if($valpass[pass] == $pass){
-			echo '<div class="text-light">Right password</div>';
-		}else{
-			echo '<div class="text-light"><?php echo $valpass?></div>';
-		}
+		echo $valpass[0];
+		//if($valpass[pass] == $pass){
+		//	echo '<div class="text-light">Right password</div>';
+		//}else{
+		//}
 	}
 ?>
