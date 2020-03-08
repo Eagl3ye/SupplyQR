e32ab8cb4cdada385fe70543b6df4f950bc26be0
Eagl3ye <vincejaphethloable@gmail.com>
2020-03-08 08:12:00 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,18 +1,14 @@
 <?php
-	$valpass = "";
 	if(isset($_POST['gateway_login'])) 
 	{
 		$email = $_POST['login_email'];
 		$pass = $_POST['login_pass'];
-		$conn = pg_connect(getenv("DATABASE_URL"));
+		$conn = pg_connect(getenv("DATABASE_URL")) or die("Unable to connect to the server: $conn\n");
 		$query = "SELECT * FROM userdata";
 
 		$rs = pg_query($conn, $query) or die("Cannot execute query: $query\n");
-		$valpass = pg_fetch_row($rs);
-		//if($valpass[pass] == $pass){
-		//	echo '<div class="text-light">Right password</div>';
-		//}else{
-		//}
+		while ($row = pg_fetch_row($rs)) {
+			echo "$row[0]\n";
+		}
 	}
-	echo $valpass[0];
 ?>
